d4426174d74b8bc0c373a1b401c959983b2486b3
OLIMJON <alimadilov55@gmail.com>
2022-12-14 20:28:16 +0500

    Initial commit

---
+++ app/src/main/java/com/example/user/MainActivity.kt
@@ -0,0 +1,29 @@
+package com.example.user
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
+import androidx.navigation.Navigation
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import com.example.user.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+    lateinit var binding: ActivityMainBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
+
+
+        val navController = Navigation.findNavController(this,R.id.fragmentContainerView)
+        setupWithNavController(binding.bottomNaw, navController)
+    }
+}
