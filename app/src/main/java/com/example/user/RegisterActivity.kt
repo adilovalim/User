55c5fe3d48f82849375c1f2125f349d999448891
OLIMJON <alimadilov55@gmail.com>
2022-12-15 18:55:16 +0500

    user version 1.1.1

--- app/src/main/java/com/example/user/RegisterActivity.kt
+++ app/src/main/java/com/example/user/RegisterActivity.kt
@@ -3,6 +3,9 @@ package com.example.user
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.user.databinding.ActivityRegisterBinding
 
 class RegisterActivity : AppCompatActivity() {
@@ -11,9 +14,14 @@ class RegisterActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityRegisterBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
 
         binding.btn.setOnClickListener {
-            val intent =  Intent(this, VerificationActivity::class.java)
+            val intent = Intent(this, VerificationActivity::class.java)
             startActivity(intent)
         }
     }
--- app/src/main/java/com/example/user/VerificationActivity.kt
+++ app/src/main/java/com/example/user/VerificationActivity.kt
@@ -3,6 +3,9 @@ package com.example.user
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.view.WindowCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.WindowInsetsControllerCompat
 import com.example.user.databinding.ActivityVerificationBinding
 
 class VerificationActivity : AppCompatActivity() {
@@ -11,6 +14,12 @@ class VerificationActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityVerificationBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
+        val windowInsetsController =
+            WindowCompat.getInsetsController(window, window.decorView) ?: return
+        windowInsetsController.systemBarsBehavior =
+            WindowInsetsControllerCompat.BEHAVIOR_SHOW_TRANSIENT_BARS_BY_SWIPE
+        windowInsetsController.hide(WindowInsetsCompat.Type.statusBars())
 
         binding.btn.setOnClickListener {
             val intent = Intent(this, MainActivity::class.java)
