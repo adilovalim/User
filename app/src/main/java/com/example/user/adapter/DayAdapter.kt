62cb563ea390265afe4db01de8a04b351cc23fa7
OLIMJON <adilovalim>
2022-12-22 21:37:47 +0500

    user version 1.2.6

---
+++ app/src/main/java/com/example/user/adapter/DayAdapter.kt
@@ -0,0 +1,36 @@
+package com.example.user.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.user.R
+import com.example.user.model.Prodact
+import kotlinx.android.synthetic.main.item_prodact.view.*
+
+class DayAdapter (var prodactList: List<Prodact>): RecyclerView.Adapter<DayAdapter.MyViewHolder>(){
+
+    inner class MyViewHolder(var itemView: View ):RecyclerView.ViewHolder(itemView){
+        fun onBind(prodact: Prodact){
+            itemView.prodact_name.text = prodact.name
+            itemView.prodact_soni.text = prodact.soni
+        }
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): MyViewHolder {
+        val itemView =LayoutInflater.from(parent.context).inflate(R.layout.item_prodact, parent,false)
+        val myViewHolder =MyViewHolder(itemView)
+        return myViewHolder
+    }
+
+    override fun onBindViewHolder(holder: MyViewHolder, position: Int) {
+        val prodact:Prodact =prodactList[position]
+        holder.onBind(prodact)
+    }
+
+    override fun getItemCount(): Int {
+        return prodactList.size
+
+    }
+}
---
+++ app/src/main/java/com/example/user/model/Prodact.kt
@@ -0,0 +1,3 @@
+package com.example.user.model
+
+data class Prodact(val id:Int? = null , val name:String? = null ,  val soni:String? = null ):java.io.Serializable
